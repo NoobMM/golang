2a41f8684a2ceb53ab94ac17677cc3d13f22bfa0
maithree01 <maithreezaa@gmail.com>
2021-04-07 17:48:09 +0700

    create repo

---
+++ app/intrastructure/repos/wallet/create_one.go
@@ -0,0 +1,30 @@
+package walletrepo
+
+import (
+	"context"
+
+	"github.com/NoobMM/golang/app/domain/entities"
+	"github.com/NoobMM/golang/app/intrastructure/models"
+)
+
+type CreateOneWalletInput struct {
+	walletEntity *entities.Wallet
+}
+
+func (repo *repo) CreateOneWallet(ctx context.Context, input CreateOneWalletInput) (*entities.Wallet, error) {
+	walletModel, err := new(models.Wallet).FormEntity(input.walletEntity)
+	if err != nil {
+		return nil, err
+	}
+	query := repo.DB
+	result := query.Create(walletModel)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	resultEntity, err := walletModel.ToEntity()
+	if err != nil {
+		return nil, err
+	}
+	return resultEntity, nil
+}
+
---
+++ app/intrastructure/repos/wallet/main.go
@@ -0,0 +1,23 @@
+package walletrepo
+
+import (
+	"context"
+
+	"github.com/NoobMM/golang/app/domain/entities"
+	"gorm.io/gorm"
+)
+
+
+type Repo interface {
+	CreateOneWallet(ctx context.Context, input CreateOneWalletInput) (*entities.Wallet, error)
+}
+
+type repo struct {
+	DB *gorm.DB
+}
+
+func New(db *gorm.DB) Repo {
+	return &repo{
+		DB: db,
+	}
+}
